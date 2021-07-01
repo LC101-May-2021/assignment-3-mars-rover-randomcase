59ea32b57465e68240f17451c8ed1922cba81901
Zakeem Mccaleb <69187509+randomcase@users.noreply.github.com>
2021-07-01 15:14:59 +0000

    Rover Tests

--- rover.js
+++ rover.js
@@ -1,5 +1,10 @@
 class Rover {
-   // Write code here!
+   constructor(position){
+     this.position = position
+     this.mode = "Normal"
+     this.generatorWatts = 110
+     this.recieveMessage(message)
+   }
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -9,5 +9,32 @@ const Command = require('../command.js');
 describe("Rover class", function() {
 
   // 7 tests here!
+it ("constructor sets position and default values for mode and generatorWatts", function() {
+  expect (rover.position).toEqual(this.position)
+  expect (rover.mode).toEqual("Normal")
+  expect (rover.generatorWatts).toEqual(110)
+})
 
-});
+it ("response returned by receive Message contains name of message", function() {
+  expect (rover.receiveMessage).toEqual(message.name)
+})
+
+it ("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+  expect (rover.receiveMessage).toEqual(message.name, message.commands)
+})
+
+it ("responds correctly to status check command", function(){
+  expect (rover.mode)
+})
+
+it ("responds correctly to mode change command", function(){
+  expect (rover.mode).toEqual(message.command)
+}
+
+it ("responds with false completed value when attempting to move in LOW_POWER mode", fucntion(){
+  expect (rover.position).toEqual()
+})
+
+it("responds with position for move command", function(){
+  expect (rover.position).toEqual()
+})})
